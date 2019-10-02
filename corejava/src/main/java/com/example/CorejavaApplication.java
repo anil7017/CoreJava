bd624f5948c91af58ca9949a3908b72baf4c8e0c
anilkumar <anilkumarsahoo17@gmail.com>
2019-10-02 19:01:26 +0530

    add Helloworld.java class

--- corejava/src/main/java/com/example/CorejavaApplication.java
+++ corejava/src/main/java/com/example/CorejavaApplication.java
@@ -3,11 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CorejavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CorejavaApplication.class, args);
+		//SpringApplication.run(CorejavaApplication.class, args);
 	}
 
 }
